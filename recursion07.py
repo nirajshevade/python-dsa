d58b1f4ea6f758b16fd50723633e6e4f216b2e5b
nirajshevade <niraj.shevade@aissmsioit.org>
2026-07-22 00:19:08 +0530

    Solving some more recursion problems

---
+++ recursion07.py
@@ -0,0 +1,10 @@
+def isPlindrome(str):
+    if len(str) <= 1:
+        return True
+    if str[0] != str[-1]:
+        return False
+    return isPlindrome(str[1:-1])
+
+
+str = input("Enter a string: ")
+print(isPlindrome(str))
