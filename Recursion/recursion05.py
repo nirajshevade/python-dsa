d998a81682051b2c0dafa08f8094d6294835b009
nirajshevade <niraj.shevade@aissmsioit.org>
2026-08-30 00:03:06 +0530

    Recursion problem solved

--- Recursion/recursion05.py
+++ Recursion/recursion05.py
@@ -1,7 +1,13 @@
+# def fact(num):
+#     if num == 0:
+#         return 1
+#     return num * fact(num-1)
+
+
 def fact(num):
-    if num == 0:
-        return 1
-    return num * fact(num-1)
+    if num > 0:
+        return num * fact(num-1)
+    else: return 1
 
 num = int(input("Enter a number: "))
 print(f"Factorial of {num}:", fact(num))
